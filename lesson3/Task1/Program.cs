fdca05ee46bbd4952a5e7fba100be36908814399
YuliYa <yuh0121@mail.ru>
2024-06-15 09:48:26 +0300

    lesson3:task1

---
+++ lesson3/Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿double CalculateFormula(int a, int b, int c, int d)
+{
+    double numenator = a * b;
+    int denomenator = c + d;
+    double result = numenator / denomenator; // чтобы сохранить дробную часть, сменим тип данных одной из переменных на тип double (сменим у numenator)
+    return result;
+}
+
+double result = CalculateFormula(1, 2, 3, 4);
+System.Console.WriteLine(result);
