59f408ab6d06372f92439fb8d5a4c400ed31cef3
YuliYa <yuh0121@mail.ru>
2024-06-16 11:04:49 +0300

    lesson4:task3

---
+++ lesson4/Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿string GetLettersFromString(string s)
+{
+    string letters = "";
+    foreach(char e in s)
+    {
+        if(char.IsAsciiLetter(e) == true) // буква ли это?
+        {
+            letters = letters + e;
+        }
+    }
+    return letters;
+}
+
+
+string str = Console.ReadLine();
+string result = GetLettersFromString(str);
+System.Console.WriteLine(result);
